cfa9a00707a9da5c5396c966b66063158647ef17
Nicolás García <nikolasg12000@gmail.com>
2026-08-23 17:15:24 -0500

    first commit

---
+++ pilo-backend/src/main/java/com/pilo/PiloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiloBackendApplication.class, args);
+	}
+
+}
